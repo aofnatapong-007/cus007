84ed99457714726f92460f5fe32abebd8105b3a6
aofnatapong-007 <684259007@webmail.npru.ac.th>
2026-03-11 11:44:42 +0700

    join

--- select_injection_solution.php
+++ select_injection_solution.php
@@ -1,7 +1,6 @@
 <?php
 require "connect.php";
 
-//$n = "1" . " or '1=1";
 $cid = $_GET["CustomerID"];
 $sql = "SELECT customer.CustomerID,
                customer.Name,
@@ -14,15 +13,30 @@ $sql = "SELECT customer.CustomerID,
         WHERE customer.CustomerID = :customerID";
 
 $stmt = $conn->prepare($sql);
-
 $stmt->bindParam(':customerID', $cid);
-
 $stmt->execute();
-
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
+// เริ่มสร้างตาราง HTML
+echo "<table border='1' style='border-collapse: collapse; width: 100%; text-align: left;'>";
+echo "<tr style='background-color: #f2f2f2;'>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Country</th>
+        <th>Debt</th>
+      </tr>";
+
 while ($row = $stmt->fetch()) {
-    echo $row['CustomerID'] . ' ' . $row['Name'] . ' ' . $row['Email'] . ' ' . $row['CountryName'] . ' ' . $row['OutstandingDebt'] . "<br/>";
+    echo "<tr>";
+    echo "<td>" . $row['CustomerID'] . "</td>";
+    echo "<td>" . $row['Name'] . "</td>";
+    echo "<td>" . $row['Email'] . "</td>";
+    echo "<td>" . $row['CountryName'] . "</td>";
+    echo "<td>" . number_format($row['OutstandingDebt'], 2) . "</td>"; // ใส่ format ตัวเลขให้ดูสวยขึ้น
+    echo "</tr>";
 }
+
+echo "</table>";
 
 $conn = null;
